67d6806ef5f2531fb7c6eefe8a622f580647ab9f
faturnawaf-arch <faturnawaf@gmail.com>
2026-05-31 16:19:39 +0700

    Update Kamera Movement
    Sudah bisa dan done

--- Assets/script/camera.cs
+++ Assets/script/camera.cs
@@ -1,40 +1,71 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraRotateSmooth : MonoBehaviour
 {
-    // Rotasi target
-    private Vector3 leftRotation = new Vector3(-9.268f, 229.94f, -5.467f);
-    private Vector3 rightRotation = new Vector3(-6.202f, 144.142f, 8.795f);
+    // Posisi kamera
+    private Vector3 leftRotation = new Vector3(-6.202f, 144.142f, 8.795f);
+    private Vector3 middleRotation = new Vector3(-8f, 185f, 0f);
+    private Vector3 rightRotation = new Vector3(-9.268f, 229.94f, -5.467f);
 
-    // Speed rotasi
     public float rotationSpeed = 2.5f;
 
     private Quaternion targetRotation;
 
+    // 0 = kiri, 1 = tengah, 2 = kanan
+    private int currentPosition = 1;
+
     void Start()
     {
-        targetRotation = transform.rotation;
+        targetRotation = Quaternion.Euler(middleRotation);
+        transform.rotation = targetRotation;
     }
 
     void Update()
     {
-        // LEFT
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.LeftArrow))
+        // ARROW KIRI
+        if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
         {
-            targetRotation = Quaternion.Euler(leftRotation);
+            if (currentPosition > 0)
+            {
+                currentPosition--;
+                UpdateTargetRotation();
+            }
         }
 
-        // RIGHT
-        if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.RightArrow))
+        // ARROW KANAN
+        if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
         {
-            targetRotation = Quaternion.Euler(rightRotation);
+            if (currentPosition < 2)
+            {
+                currentPosition++;
+                UpdateTargetRotation();
+            }
         }
 
-        // Smooth rotation seperti orang menoleh
+        // Smooth rotation
         transform.rotation = Quaternion.RotateTowards(
             transform.rotation,
             targetRotation,
             rotationSpeed * 100f * Time.deltaTime
         );
+    }
+
+    void UpdateTargetRotation()
+    {
+        switch (currentPosition)
+        {
+            case 0:
+                targetRotation = Quaternion.Euler(leftRotation);
+                break;
+
+            case 1:
+                targetRotation = Quaternion.Euler(middleRotation);
+                break;
+
+            case 2:
+                targetRotation = Quaternion.Euler(rightRotation);
+                break;
+        }
     }
 }
